938592057543644439e6903a8c7b4f5967d969a6
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2023-10-06 22:13:16 +0300

    navigate using Future.delay

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/core/utils/app_strings.dart';
-import 'package:to_do_app/featuers/auth/presentation/screens/splash_screen/splash_screen.dart';
+import 'package:to_do_app/features/auth/presentation/screens/splash_screen/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/features/auth/presentation/screens/splash_screen/splash_screen.dart
+++ lib/features/auth/presentation/screens/splash_screen/splash_screen.dart
@@ -2,11 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:to_do_app/core/utils/app_colors.dart';
 import 'package:to_do_app/core/utils/app_strings.dart';
+import 'package:to_do_app/features/auth/presentation/screens/on_boarding_screens/on_boarding_screens.dart';
 
 import '../../../../../core/utils/app_assets.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    navigate();
+  }
+
+  void navigate() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (_) => const OnBoardingScreens()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {

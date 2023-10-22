724d838718743cb7c4c36f1b54c8ed5c80aa9a23
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2023-10-22 21:44:22 +0300

    Date Picker Timeline

--- lib/features/task/presentation/screens/home_screen.dart
+++ lib/features/task/presentation/screens/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:to_do_app/core/utils/app_colors.dart';
+import 'package:to_do_app/core/utils/app_strings.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -13,16 +16,45 @@ class HomeScreen extends StatelessWidget {
           backgroundColor: AppColors.primary,
           child: const Icon(Icons.add),
         ),
-        body: Column(
-          children: [
-            Text(
-              DateTime.now().toString(),
-              style: TextStyle(
-                fontSize: 32.0,
-                color: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                DateFormat.yMMMd().format(DateTime.now()),
+                style: Theme.of(context)
+                    .textTheme
+                    .displayLarge!
+                    .copyWith(fontSize: 24.0),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 12.0,
+              ),
+              Text(
+                AppStrings.today,
+                style: Theme.of(context)
+                    .textTheme
+                    .displayLarge!
+                    .copyWith(fontSize: 24.0),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  DatePicker(
+                    DateTime.now(),
+                    initialSelectedDate: DateTime.now(),
+                    selectionColor: AppColors.primary,
+                    dayTextStyle: Theme.of(context).textTheme.displayMedium!,
+                    dateTextStyle: Theme.of(context).textTheme.displayMedium!,
+                    monthTextStyle: Theme.of(context).textTheme.displayMedium!,
+                    selectedTextColor: AppColors.white,
+                    onDateChange: (date) {},
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
